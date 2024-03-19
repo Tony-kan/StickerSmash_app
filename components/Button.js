948aefa55213e428b42ec5dfdf6956a6daa56449
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-03-19 13:26:06 +0200

    build the StickerSmash Screen

---
+++ components/Button.js
@@ -0,0 +1,73 @@
+import { StyleSheet, Text, Pressable, View } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export const Button = ({ label, theme }) => {
+  if (theme === "primary") {
+    return (
+      <View style={styles.themedButtonContainer}>
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You Pressed a CHoose Photo Button Button.")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.ButtonIcon}
+          />
+          <Text style={[styles.ButtonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("You Pressed a use this Photo Button.")}
+      >
+        <Text style={styles.ButtonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  themedButtonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+    borderWidth: 4,
+    borderColor: "#ffd33d",
+    borderRadius: 18,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  ButtonIcon: {
+    paddingRight: 8,
+  },
+  ButtonLabel: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
---
+++ components/ImageViewer.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Image } from "react-native";
+
+export default ImageViewer = ({ placeHolderImage }) => {
+  return <Image source={placeHolderImage} style={styles.image} />;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
